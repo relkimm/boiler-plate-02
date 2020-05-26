36db0ec70837e844b7954bc01ff8a4f9362b5e47
chillgu <bbonggu92@gmail.com>
2020-05-26 22:50:17 +0900

    added body-parser & nodemon'

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const { User } = require('./models/User');
+
+app.listen(port, () => console.log(`express app listening on port ${port}!`));
 
 mongoose.connect('mongodb+srv://chillgu:abcd1234@boiler-plate-12xps.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -11,7 +16,16 @@ mongoose.connect('mongodb+srv://chillgu:abcd1234@boiler-plate-12xps.mongodb.net/
     useFindAndModify: false
 }).then(() => console.log('MongoDb connected...')).catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.get('/', (req, res) => res.send('hello nodemon!'));
+app.post('/register', (req, res) => {
 
-app.get('/', (req, res) => res.send('hello express!'));
-app.listen(port, () => console.log(`express app listening on port ${port}!`));
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+
+        return res.status(200).json({ success: true });
+    });
+})
--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         trim: true,
-        unique: 1
+        unique: true
     },
     password: {
         type: String,
